7e238057d86ab50ac044aafcca0e6bdcd44d9b50
Danilo Lopes <daniloinfinitbr@gmail.com>
2023-03-02 17:43:10 -0300

    Processamento de empregados usando uma lista

--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class TesteEmpregado {
     public static void main(String[] args) {
         Empregado e1 = new Empregado();
@@ -14,5 +16,20 @@ public class TesteEmpregado {
         e3.setSalario(1700);
         e3.setBonus(400);    
 
+        var empregados = new ArrayList<Empregado>();
+        empregados.add(e1);
+        empregados.add(e2);
+        empregados.add(e3);
+
+        //enhaced for (Java 5+)
+        for (Empregado e : empregados){
+        System.out.println(e.calcularSalario());
+        }
+
+        //desafio: reescreva o enhaced for usando um for regular
+
+        for (int i = 0; i < empregados.size(); i++) {
+            System.out.println(empregados.get(i).calcularSalario());
+        }
     }
 }
